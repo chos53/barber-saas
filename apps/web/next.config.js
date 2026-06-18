6a8533468058147a35ea4d7bcff7c7aa44830d5b
Carlos Henrique <carloshenr@hotmail.com>
2026-06-18 02:50:12 -0300

    fix: removendo configuração de eslint inválida do next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! ATENÇÃO: Isso ignora erros de tipo no build !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // !! ATENÇÃO: Isso ignora erros de lint no build !!
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
   },
 };
-
 module.exports = nextConfig;
